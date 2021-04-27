88eb6fe7b9feb7c42bb44a4e5fa0cebd44593eb9
ziosanfer <ziosanfer@gmail.com>
2021-04-27 14:28:29 -0700

    Wrote up the User model and made the table

--- w7d2/Zap/app/models/user.rb
+++ w7d2/Zap/app/models/user.rb
@@ -1,2 +1,37 @@
 class User < ApplicationRecord
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+
+        return user if user && user.is_password(password)
+        nil
+    end
+
+    def self.generate_session_token
+        @sesh_token = SecureRandom::urlsafe_base64
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save
+        self.session_token
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_obj = BCrypt::Password.create(password)
+        password_obj.is_password?(password) 
+        #isn't recrusive, is calling the is_password? that BCrypt has which is why
+        #we create and set the password_obj to an instance of BCrypt Password
+
+    end
 end
