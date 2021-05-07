31e3d82c18dac7d847bd20986c41c3ed75bddfc3
ziosanfer <ziosanfer@gmail.com>
2021-05-07 14:21:00 -0700

    made a clock

--- w8d5/Callback_Excercises/clock.js
+++ w8d5/Callback_Excercises/clock.js
@@ -1,9 +1,30 @@
 class Clock {
     constructor() {
-        
+        this.date = new Date();
+        this.hour = this.date.getHours();
+        this.minute = this.date.getMinutes();
+        this.second = this.date.getSeconds();
+        this._tick = this._tick.bind(this)
+        setInterval(this._tick, 1000);
+    }
+
+    printTime() {
+        console.log(`${this.hour}:${this.minute}:${this.second}`);
+    }
+
+    _tick() {
+        this.second += 1;
+
+        if (this.second === 60) {
+            this.minute += 1;
+            this.second = 0;
+        } else if (this.minute === 60) {
+            this.hour += 1;
+            this.minute = 0;
+        } 
+        this.hour = this.hour % 24;
+        this.printTime();
     }
 }
 
-// printTime() {
-
-// }
+const clock = new Clock ();
